10e037a48941a24d7592f3859bb02655be63ad14
LD-dongdong-520 <52145226+LD-dongdong-520@users.noreply.github.com>
2020-03-03 20:04:51 +0800

    hello world
    第一次使用GitHub上次文件

---
+++ 01 Hello World.cpp
@@ -0,0 +1,13 @@
+#include<iostream>//标准输入输出流
+
+using namespace std;//使用命名空间，打开一个std的房间
+
+int main()
+{
+	//cout标准输出
+	//<<左移运算符，运算符的重载
+	//endl结束换行
+	cout << "Hello World!"<<endl;
+	system("pause");//阻塞功能
+	return EXIT_SUCCESS;//返回正常的退出
+}
